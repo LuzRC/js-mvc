668958b6702b31a71a36c56c5865eb7ff87ccf5f
Víctor Velarde <victor.velarde@gmail.com>
2017-03-08 23:19:40 +0100

    inicio

---
+++ TaskView.js
@@ -0,0 +1,29 @@
+var TaskView = function (model) {
+    this.model = model;
+    this.addTaskEvent = new Event(this);
+    this.selectTaskEvent = new Event(this);
+    this.unselectTaskEvent = new Event(this);
+    this.completeTaskEvent = new Event(this);
+    this.deleteTaskEvent = new Event(this);
+
+    this.init();
+};
+
+TaskView.prototype = {
+    init: function () {
+        this.createChildren()
+            .setupHandlers()
+            .enable();
+    },
+
+    createChildren: function () {
+        this.$container = $('.js-container');
+        this.$addTaskButton = this.$container.find('.js-add-task-button');
+        this.$taskTextBox = this.$container.find('.js-task-textbox');
+        this.$tasksContainer = this.$container.find('.js-tasks-container');
+
+        return this;
+    }
+}
+
+/*... in progress...*/
